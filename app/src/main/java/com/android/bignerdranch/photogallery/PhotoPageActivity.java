b4decddf5953ab4db57bf5a727bbf3e0f0123c7d
Vlad <0vsyannikovv24@gmail.com>
2020-05-15 18:05:28 +0700

    Использование кнопки Back для работы с историей просмотра

--- app/src/main/java/com/android/bignerdranch/photogallery/PhotoPageActivity.java
+++ app/src/main/java/com/android/bignerdranch/photogallery/PhotoPageActivity.java
@@ -3,6 +3,7 @@ package com.android.bignerdranch.photogallery;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.webkit.WebView;
 
 import androidx.fragment.app.Fragment;
 
@@ -17,5 +18,16 @@ public class PhotoPageActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
         return PhotoPageFragment.newInstance(getIntent().getData());
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) findViewById(R.id.web_view);
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
